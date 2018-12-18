d5a1fe15ae0cf1e7291d1260dddafa15e8d086d3
Baher Abdelfatah <36481333+BaherAbdelfatah@users.noreply.github.com>
2018-12-18 13:06:10 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ function myFunction() {
   document.getElementById("year").innerHTML = document.getElementById("bookyear").value;
   document.getElementById("Publisher").innerHTML = document.getElementById("bookPublisher").value;
   document.getElementById("Author").innerHTML = document.getElementById("bookAuthor").value;
-x = document.getElementById("Background").options.value;
+
+var x = document.getElementById("Background").value;
 if(x=="red")
-document.getElementById("book").body.style.backgroundImage = "url('red.jpg')";
+document.getElementById("book").style.backgroundImage = "url('red.jpg')";
 else if(x=="brown")
-document.getElementById("book").body.style.backgroundImage = "url('brown.jpg')";
+document.getElementById("book").style.backgroundImage = "url('brown.jpg')";
 else if(x=="blue")
-document.getElementById("book").body.style.backgroundImage = "url('blue.jpg')";
+document.getElementById("book").style.backgroundImage = "url('blue.jpg')";
 else if(x=="orange")
-document.getElementById("book").body.style.backgroundImage = "url('orange.jpg')";
+document.getElementById("book").style.backgroundImage = "url('orange.jpg')";
 }
